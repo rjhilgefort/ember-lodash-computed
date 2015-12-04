8727443ebed574658800db53f3051234fbd6be98
Rob Hilgefort <rjhilgefort@gmail.com>
2015-12-04 12:00:34 -0500

    Added macro blueprint

---
+++ blueprints/macros/files/__root__/macros/__name__.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+const { computed } = Ember;
+
+/**
+ * Description [TODO]
+ *
+ * @name <%= camelizedModuleName %>
+ * @param {...String} dependentKeys Argument list of dependent keys
+ * @return {Ember.ComputedProperty} computed property which returns ...[TODO]
+ */
+export default function <%= camelizedModuleName %>(...dependentKeys) {
+  return computed(dependentKeys, function() {
+    return this.getProperties(dependentKeys);
+  });
+}
---
+++ blueprints/macros/index.js
@@ -0,0 +1,10 @@
+/**
+ * EX: `ember generate macro foo`
+ */
+module.exports = {
+  description: 'Generates an ember-lodash-macros macro.',
+
+  locals: function(options) {
+    return options;
+  }
+};
