37c3b074ea5e632b65f51fd5c15bb9fda6e12267
Rob Hilgefort <rjhilgefort@gmail.com>
2015-12-10 14:26:13 -0500

    export all the things

--- addon/index.js
+++ addon/index.js
@@ -1,2 +1,53 @@
+import isArguments from './computeds/is-arguments';
+export { isArguments };
+
+import isArray from './computeds/is-array';
+export { isArray };
+
+import isBoolean from './computeds/is-boolean';
+export { isBoolean };
+
+import isDate from './computeds/is-date';
+export { isDate };
+
+import isElement from './computeds/is-element';
+export { isElement };
+
+import isEmpty from './computeds/is-empty';
+export { isEmpty };
+
+import isError from './computeds/is-error';
+export { isError };
+
+import isFunction from './computeds/is-function';
+export { isFunction };
+
+import isNaN from './computeds/is-nan';
+export { isNaN };
+
+import isNative from './computeds/is-native';
+export { isNative };
+
+import isNull from './computeds/is-null';
+export { isNull };
+
+import isNumber from './computeds/is-number';
+export { isNumber };
+
+import isObject from './computeds/is-object';
+export { isObject };
+
+import isPlainObject from './computeds/is-plain-object';
+export { isPlainObject };
+
+import isRegExp from './computeds/is-reg-exp';
+export { isRegExp };
+
 import isString from './computeds/is-string';
 export { isString };
+
+import isTypedArray from './computeds/is-typed-array';
+export { isTypedArray };
+
+import isUndefined from './computeds/is-undefined';
+export { isUndefined };
