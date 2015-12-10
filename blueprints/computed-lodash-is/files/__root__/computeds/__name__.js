c573742a4aff7f10a32e0326e038a8c6615c1813
Rob Hilgefort <rjhilgefort@gmail.com>
2015-12-10 11:43:12 -0500

    added blueprint to generate lodash is methods

---
+++ blueprints/computed-lodash-is/files/__root__/computeds/__name__.js
@@ -0,0 +1,16 @@
+import _ from 'lodash/lodash';
+import Ember from 'ember';
+const { computed } = Ember;
+
+/**
+ * If one or more properties satisfy `<%= camelizedModuleName %>`
+ *
+ * @name <%= camelizedModuleName %>
+ * @param {...String} dependentKeys Argument list of dependent keys
+ * @return {Ember.ComputedProperty} computed property
+ */
+export default function <%= camelizedModuleName %>(...dependentKeys) {
+  return computed(dependentKeys, function() {
+    return _.every(this.getProperties(dependentKeys), _.<%= camelizedModuleName %>);
+  });
+}
---
+++ blueprints/computed-lodash-is/index.js
@@ -0,0 +1,10 @@
+/**
+ * EX: `ember generate computed-lodash-is foo`
+ */
+module.exports = {
+  description: 'Generates an ember-lodash-computed computed for `is` method.',
+
+  locals: function(options) {
+    return options;
+  }
+};
